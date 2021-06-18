6a29bcbf4617d99124b8196d29bbcdad2ec06f67
Paul Blackwell <blackwellpaul34@gmail.com>
2021-06-18 18:32:58 +0100

    Display Dashboard Events

--- dj-events-frontend/pages/account/dashboard.js
+++ dj-events-frontend/pages/account/dashboard.js
@@ -1,13 +1,24 @@
 import { parseCookies } from "@/helpers/index";
 import Layout from "@/components/Layout";
+import DashboardEvent from "@/components/DashboardEvent";
 import { API_URL } from "@/config/index";
+import styles from "@/styles/Dashboard.module.css";
 
-export default function DashboardPage({events}) {
+export default function DashboardPage({ events }) {
 
-  console.log(events);
+  const deleteEvent = (id) => {
+    console.log(id)
+  }
+
   return (
     <Layout title="User Dashboard">
-      <h1>Dashboard</h1>
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My events</h3>
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div>
     </Layout>
   );
 }
@@ -16,16 +27,15 @@ export async function getServerSideProps({ req }) {
   const { token } = parseCookies(req);
 
   const res = await fetch(`${API_URL}/events/me`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      Authorization: `Bearer ${token}`
-    }
-
+      Authorization: `Bearer ${token}`,
+    },
   });
 
-  const events = await res.json()
+  const events = await res.json();
 
   return {
-    props: {events},
+    props: { events },
   };
 }
