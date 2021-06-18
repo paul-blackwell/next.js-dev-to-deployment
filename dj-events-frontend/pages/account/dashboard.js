2ba86f616d5967af4f7fea90f37eadbad531b2f4
Paul Blackwell <blackwellpaul34@gmail.com>
2021-06-18 20:20:48 +0100

    Authenticated Edit, Delete & Image Upload

--- dj-events-frontend/pages/account/dashboard.js
+++ dj-events-frontend/pages/account/dashboard.js
@@ -1,14 +1,31 @@
+import { useRouter } from "next/router";
 import { parseCookies } from "@/helpers/index";
 import Layout from "@/components/Layout";
 import DashboardEvent from "@/components/DashboardEvent";
 import { API_URL } from "@/config/index";
 import styles from "@/styles/Dashboard.module.css";
 
-export default function DashboardPage({ events }) {
+export default function DashboardPage({ events, token }) {
+  const router = useRouter();
 
-  const deleteEvent = (id) => {
-    console.log(id)
-  }
+  const deleteEvent = async (id) => {
+    if (confirm("Are you sure?")) {
+      const res = await fetch(`${API_URL}/events/${id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message);
+      } else {
+        router.reload("/events");
+      }
+    }
+  };
 
   return (
     <Layout title="User Dashboard">
@@ -36,6 +53,9 @@ export async function getServerSideProps({ req }) {
   const events = await res.json();
 
   return {
-    props: { events },
+    props: {
+      events,
+      token,
+    },
   };
 }
