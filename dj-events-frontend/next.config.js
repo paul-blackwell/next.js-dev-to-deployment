eb82f225b34ca3d92ef68db130a973999382e1be
Paul Blackwell <blackwellpaul34@gmail.com>
2021-06-19 13:49:12 +0100

    Testing bug fix for CORS issue

--- dj-events-frontend/next.config.js
+++ dj-events-frontend/next.config.js
@@ -1,13 +1,26 @@
 module.exports = {
-  async rewrites() {
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  async headers() {
     return [
       {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'}:path*`,
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
-    ]
+    ];
   },
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-}
+};
