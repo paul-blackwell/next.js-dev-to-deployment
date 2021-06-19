f85e24dc4b2b3c069e3e67137480c474e98a021f
Paul Blackwell <blackwellpaul34@gmail.com>
2021-06-19 13:12:41 +0100

    Had to add to the next.config to fix a CORS issue

--- dj-events-frontend/next.config.js
+++ dj-events-frontend/next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'}:path*`,
+      },
+    ]
+  },
   images: {
     domains: ['res.cloudinary.com'],
   },
